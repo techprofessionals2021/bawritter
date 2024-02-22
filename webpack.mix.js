1e0e107775dee4b8d2bb87b3f3b6dde5a0df4a5b
syedhammadali1 <hammadali407292@gmail.com>
2024-02-22 20:29:05 +0500

    show pagination

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,11 @@
 const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js').vue().sass('resources/sass/app.scss', 'public/css/app.min.css');
-// mix.sass('resources/sass/theme.scss', 'public/css/theme.min.css');
+mix.sass('resources/sass/theme.scss', 'public/css/theme.min.css');
 // mix.sass('resources/sass/app.scss', 'public/css/app.min.css');
-// mix.sass('resources/sass/authentication.scss', 'public/css/authentication.css');
-// mix.js('resources/js/theme.min.js', 'public/js');
-// mix.sass('resources/sass/invoice.scss', 'public/css/invoice.css');
+mix.sass('resources/sass/authentication.scss', 'public/css/authentication.css');
+mix.js('resources/js/theme.min.js', 'public/js');
+mix.sass('resources/sass/invoice.scss', 'public/css/invoice.css');
 // mix.scripts([
 //     'resources/js/theme.js',
 //     'resources/js/website/jquery.slicknav.min.js',
