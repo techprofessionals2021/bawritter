4c1ecbee44b85f7b89037de32bb1e8a1dca6272d
syedhammadali1 <hammadali407292@gmail.com>
2024-03-19 06:24:47 +0500

    .

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\models\User;
+use App\Models\User;
 use Exception;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
