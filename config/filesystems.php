98ac53ae94d70bce21e273a7f9bba1bd2d087593
syedhammadali1 <hammadali407292@gmail.com>
2024-08-15 17:21:23 +0500

    change file system config

--- config/filesystems.php
+++ config/filesystems.php
@@ -79,8 +79,7 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('app/public'),
-        public_path('attachments') => storage_path('app/attachments'),
+        public_path('storage') => storage_path('app'),
     ],
 
 ];
