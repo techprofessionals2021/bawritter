b87bc352781816e59e2f99bd7e2422052777f7a2
syedhammadali1 <hammadali407292@gmail.com>
2024-12-10 13:34:47 +0500

    comments

---
+++ app/Http/Resources/Comment/CommentResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\Comment;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CommentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray($request)
+    {
+        $fullname = @$this->user->first_name .' '. @$this->user->last_name;
+        return [
+            'id' => @$this->id,
+            'body' => @$this->body,
+            'user_name' => $fullname,
+            'created_at' => @$this->created_at
+        ];
+    }
+}
